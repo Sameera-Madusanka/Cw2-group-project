4cf0d1f7044b39488dea57017b6c1f9119b51221
Sameera Madusanka <ganwarige.20211120@iit.ac.lk>
2022-12-20 23:33:02 +0530

    Character controller done

--- Unity group project/Assets/Sameera/scripts/animationStateController.cs
+++ Unity group project/Assets/Sameera/scripts/animationStateController.cs
@@ -6,11 +6,16 @@ public class animationStateController : MonoBehaviour
 {
     Animator animator;
     int isWalkingHash;
+    int leftWalkingHash;
+    int rightWalkingHash;
+
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         isWalkingHash = Animator.StringToHash("isWalking");
+        leftWalkingHash = Animator.StringToHash("leftWalking");
+        rightWalkingHash = Animator.StringToHash("rightWalking");
     }
 
     // Update is called once per frame
@@ -30,5 +35,52 @@ public class animationStateController : MonoBehaviour
             //then set the isWalking boolean to be false
             animator.SetBool(isWalkingHash, false);
         }
+
+
+        bool leftWalking = animator.GetBool(leftWalkingHash);
+        bool leftPressed = Input.GetKey("a");
+        //if player presses a key
+        if (!leftWalking && leftPressed)
+        {
+            //then set the isWalking boolean to be true
+            animator.SetBool(leftWalkingHash, true);
+
+        }
+        if (leftWalking && !leftPressed)
+        {
+            //then set the isWalking boolean to be false
+            animator.SetBool(leftWalkingHash, false);
+        }
+
+        bool rightWalking = animator.GetBool(rightWalkingHash);
+        bool rightPressed = Input.GetKey("d");
+        //if player presses d key
+        if (!rightWalking && rightPressed)
+        {
+            //then set the isWalking boolean to be true
+            animator.SetBool(rightWalkingHash, true);
+
+        }
+        if (rightWalking && !rightPressed)
+        {
+            //then set the isWalking boolean to be false
+            animator.SetBool(rightWalkingHash, false);
+        }
+
+        /*
+        bool backPressed = Input.GetKey("s");
+        //if player presses d key
+        if (!isWalking && backPressed)
+        {
+            //then set the isWalking boolean to be true
+            animator.SetBool(isWalkingHash, true);
+
+        }
+        if (isWalking && !backPressed)
+        {
+            //then set the isWalking boolean to be false
+            animator.SetBool(isWalkingHash, false);
+        } */
+
     }
 }
